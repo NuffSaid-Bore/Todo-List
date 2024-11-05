acc1cb76d2531e929d25335e729cbb252ce9051c
NuffSaid-Bore <borekamohelo@gmail.com>
2024-11-05 13:34:05 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+// mongodb+srv://borekamohelo:<4g1AYOJKAKMeyXpd>@cluster0.mj2km.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+const express = require('express')
+const app = express()
+
+app.listen(3000, ()=> {
+    console.log('App is Running');
+})
