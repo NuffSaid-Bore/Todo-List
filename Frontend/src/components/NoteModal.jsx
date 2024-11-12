28c759438953cb9ca033ad5d8c3188a14ca28d4f
NuffSaid-Bore <borekamohelo@gmail.com>
2024-11-12 09:37:57 +0200

    changed the modal to be responsive for smaller screens

--- Frontend/src/components/NoteModal.jsx
+++ Frontend/src/components/NoteModal.jsx
@@ -34,53 +34,55 @@ const NoteModal = ({closeModal, addNote, currentNote, editNote}) => {
     
     return (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-15 flex justify-center items-center z-50 transition-opacity duration-300 ease-in-out hover:opacity-100">
-            <div className="bg-white p-8 rounded-lg relative w-full max-w-lg">
-                <button 
-                    className="absolute top-5 right-5 text-gray-600 hover:text-purple-400 font-bold"
-                    onClick={closeModal} 
-                >
-                    X
-                </button>
-
-                <h2 className="text-xl font-bold mb-4 text-purple-400 hover:text-grey-400">{currentNote ? "Update Note" : "Add New Note"}</h2>
-                <form onSubmit={handleSubmit}>
+            <div className="bg-white p-4 sm:p-6 md:p-8 rounded-lg relative w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto">
+            <button 
+                className="absolute top-3 right-3 sm:top-5 sm:right-5 text-gray-600 hover:text-purple-400 font-bold"
+                onClick={closeModal} 
+            >
+                X
+            </button>
+    
+            <h2 className="text-lg sm:text-xl font-bold mb-4 text-purple-400 hover:text-grey-400">
+                {currentNote ? "Update Note" : "Add New Note"}
+            </h2>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Enter Note Title"
+                    className="border p-2 w-full mb-4 rounded-md"
+                />
+                <textarea
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    placeholder="Enter Note Description"
+                    className="border p-2 w-full mb-4 rounded-md"
+                />
+                
+                <div className="flex items-center mb-4">
                     <input 
-                        type="text" 
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Enter Note Title"
-                        className="border p-2 w-full mb-4 rounded-md"
-                    />
-                    <textarea
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        placeholder="Enter Note Description"
-                        className="border p-2 w-full mb-4 rounded-md"
-                    />
-                    
-                    <div className="flex items-center mb-4">
-                        <input 
                         id="default-checkbox" 
                         type="checkbox" 
                         checked={isComplete}
                         onChange={handleCheckboxChange} 
-                        className="w-4 h-4 text-purple-600 bg-purple-100 border-purple-300 rounded focus:ring-purple-500 dark:focus:ring-purple-600 dark:ring-offset-purple-800 focus:ring-2 dark:bg-purple-700 dark:border-purple-600"/>
-                        <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-purple-900 dark:text-purple-300">Complete</label>
-                    </div>
-
-
-                    <div className="flex justify-end mt-4">
-                        <button 
-                            type="submit"
-                            className="px-4 py-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg"
-                        >
-                           {currentNote ? "Update Note" : "Add Note"} 
-                        </button>
-                    </div>
-                </form>
-            </div>
+                        className="w-4 h-4 text-purple-600 bg-purple-100 border-purple-300 rounded focus:ring-purple-500 dark:focus:ring-purple-600 dark:ring-offset-purple-800 focus:ring-2 dark:bg-purple-700 dark:border-purple-600"
+                    />
+                    <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-purple-900 dark:text-purple-300">Complete</label>
+                </div>
+    
+                <div className="flex justify-end mt-4">
+                    <button 
+                        type="submit"
+                        className="px-4 py-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg"
+                    >
+                        {currentNote ? "Update Note" : "Add Note"} 
+                    </button>
+                </div>
+            </form>
         </div>
-
+    </div>
+    
     )
 }
 
